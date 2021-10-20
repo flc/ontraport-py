7b56bb370f09f9fa477de7a5c02d7533bdbb2d6f
Zoltan Szalai <flc@users.noreply.github.com>
2021-10-21 01:02:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     version="0.1",
     packages=find_packages(),
     author="Zoltan Szalai",
-    author_email="defaultdict@gmail.com",
     description="Python client for ONTRAPORT API",
     url="https://github.com/flc/ontraport-py",
 )
